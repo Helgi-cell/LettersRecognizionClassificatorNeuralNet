b61de2acb66e6127e6470ea351f9f365bd9c221a
Helgi-cell <olegsuhodolsky@gmail.com>
2023-06-06 20:37:00 +0300

    #4

--- LettersRecognizionClassificatorNeuralNet/src/Image.java
+++ LettersRecognizionClassificatorNeuralNet/src/Image.java
@@ -17,24 +17,32 @@ public class Image {
                                          "a1.png", "a2.png","a3.png","a4.png","a5.png","a6.png","a7.png","a8.png",
                                          "a9.png", "a10.png","a11.png","a12.png","a13.png","a14.png","a15.png","a16.png"
                                         };
-        Character letter = 'a';
+        //Character letter = 'a';
+        Character[] letters = new Character[] { 'a', 'o', 'i' , 't', 'm' , 'w'
 
-        List<Double> outByLetter = new ArrayList<>();
-        outByLetter.add(100.0d);
+                                                };
+
+
+
+        for (Character letter : letters) {
+
+            List<Double> outByLetter = new ArrayList<>();
+            outByLetter.add(100.0d);
 
 
             //OperatingImage picture = new OperatingImage(files);
             OperatingImage picture = new OperatingImage(letter);
             List<NeuralNetForImages> arrTemplates = new ArrayList<>();
 
-            for (List<Double> input : picture.learnInputData){
+            for (List<Double> input : picture.learnInputData) {
                 arrTemplates.add(new NeuralNetForImages(letter, input, outByLetter));
             }
 
-        ImagesClassificatorSerializator.writeInputDataToFile(arrTemplates, "learnClass_a.net");
-        arrTemplates = ImagesClassificatorSerializator.getInputDataFromFile("learnClass_a.net");
+            ImagesClassificatorSerializator.writeInputDataToFile(arrTemplates, "learnClass_" + letter + ".net");
+            arrTemplates = ImagesClassificatorSerializator.getInputDataFromFile("learnClass_" + letter + ".net");
 
-        System.out.println(arrTemplates);
+            System.out.println(arrTemplates);
+        }
 
     }
 
